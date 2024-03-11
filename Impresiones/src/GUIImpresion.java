5733c53b00d32ef50b39c2c9df5c13a57a64f36b
DafneCruz03 <dafnecruzaguilar03@gmail.com>
2024-03-10 21:30:09 -0600

    Crear proyecto, hacer GUI e iniciar repositorio (Dafne Cruz)

--- Impresiones/src/GUIImpresion.java
+++ Impresiones/src/GUIImpresion.java
@@ -7,11 +7,13 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JList;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.ImageIcon;
 
 public class GUIImpresion extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -34,56 +36,39 @@ public class GUIImpresion extends JFrame {
 	 */
 	public GUIImpresion() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 637, 406);
+		setBounds(100, 100, 419, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		textField = new JTextField();
-		textField.setBounds(10, 10, 282, 24);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		JButton btnMandar = new JButton("Mandar archivo a la cola de impresion");
+		btnMandar.setBounds(29, 167, 249, 21);
+		contentPane.add(btnMandar);
 		
-		JButton btnNewButton = new JButton("Mandar");
-		btnNewButton.setBounds(10, 60, 97, 21);
-		contentPane.add(btnNewButton);
-		
-		JButton btnImprimir = new JButton("Imprimir");
-		btnImprimir.setBounds(117, 60, 85, 21);
-		contentPane.add(btnImprimir);
-		
-		JButton btnNewButton_1_1 = new JButton("Vaciar");
-		btnNewButton_1_1.setBounds(212, 60, 80, 21);
-		contentPane.add(btnNewButton_1_1);
-		
-		JList list = new JList();
-		list.setBounds(315, 14, 298, 345);
-		contentPane.add(list);
-		
-		JLabel lblNewLabel = new JLabel("(Imagen impresora)");
-		lblNewLabel.setBounds(10, 107, 282, 108);
-		contentPane.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("Primero:");
-		lblNewLabel_1.setBounds(10, 238, 97, 13);
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon("src/IMPRESORA.png"));
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setBounds(102, 48, 205, 109);
 		contentPane.add(lblNewLabel_1);
 		
-		JLabel lblNewLabel_1_1 = new JLabel("Ultimo:");
-		lblNewLabel_1_1.setBounds(10, 261, 97, 13);
-		contentPane.add(lblNewLabel_1_1);
+		JLabel lblNewLabel = new JLabel("Impresora Epson-L3250");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNewLabel.setBounds(102, 25, 205, 13);
+		contentPane.add(lblNewLabel);
 		
-		JLabel lblNewLabel_1_1_1 = new JLabel("Pendientes:");
-		lblNewLabel_1_1_1.setBounds(10, 284, 97, 13);
-		contentPane.add(lblNewLabel_1_1_1);
+		JButton btnImp = new JButton("Imprimir");
+		btnImp.setBounds(288, 167, 91, 21);
+		contentPane.add(btnImp);
 		
-		JLabel lblNewLabel_1_1_1_1 = new JLabel("Esta llena:");
-		lblNewLabel_1_1_1_1.setBounds(10, 307, 97, 13);
-		contentPane.add(lblNewLabel_1_1_1_1);
+		JButton btnVaciar = new JButton("Vaciar");
+		btnVaciar.setBounds(288, 205, 91, 21);
+		contentPane.add(btnVaciar);
 		
-		JLabel lblNewLabel_1_1_1_1_1 = new JLabel("Esta vacía:");
-		lblNewLabel_1_1_1_1_1.setBounds(10, 330, 97, 13);
-		contentPane.add(lblNewLabel_1_1_1_1_1);
+		JButton btnVer = new JButton("Cola de impresion (pendientes)");
+		btnVer.setBounds(29, 205, 249, 21);
+		contentPane.add(btnVer);
 	}
 }
