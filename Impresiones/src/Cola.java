9f15f62db8a70b62ec54241982fa70cf0ae02eee
EsmeraldaColiaza <ezmeraldiithaa29@gmail.com>
2024-03-11 00:09:04 -0600

    se creo el constructor, con el inicio y el final vacio y se puso el primer metodo cola vacia (esmeralda aplomino)

--- Impresiones/src/Cola.java
+++ Impresiones/src/Cola.java
@@ -2,4 +2,18 @@
 public class Cola {
 	private Nodo inicioCola, finalCola;
 	String Cola = "";  //atributos 
+	
+	public Cola() {
+		inicioCola = null;
+		finalCola = null;
+	}//constructor1
+	
+	//colavacia
+	public boolean ColaVacia() {
+		if(inicioCola==null) {
+			return true;
+		}else {
+			return false;
+		}
+	}//finMetodo la cola vacia nos servira para verificar que no haya archivos pendientes 
 }
